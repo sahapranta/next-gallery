aa85aebfce6ae4d83a8e857044806f0460f7ca05
sahapranta <pranta1204@gmail.com>
2024-04-17 02:12:17 +0600

    added nav icon

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -6,16 +6,21 @@ import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
     return (
-        <nav className="flex items-center justify-between w-full border-b px-6 py-4 text-xl font-semibold">
-            <div>Next Gallery</div>
-            <div className="flex items-center gap-4">
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <SimpleUploadButton />
-                    <UserButton />
-                </SignedIn>
+        <nav className="w-full">
+            <div className="flex items-center justify-between mt-4 mx-6 border shadow-inner shadow-sky-500 px-6 py-4 text-xl font-semibold rounded-md bg-slate-900/20 backdrop-blur-md">
+                <div className="flex items-center gap-4">
+                    <img src="/image.png" alt="logo" width={35} />
+                    Next Gallery
+                </div>
+                <div className="flex items-center gap-6">
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <SimpleUploadButton />
+                        <UserButton />
+                    </SignedIn>
+                </div>
             </div>
         </nav>
     )
